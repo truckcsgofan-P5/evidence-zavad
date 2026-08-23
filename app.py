00bdf61128dceb3f9b6da4441a6a61562f412815
truckcsgofan-P5 <truckcsgofan@gmail.com>
2026-08-23 13:26:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -433,7 +433,7 @@ with tab_prehled:
                     row["Lokomotiva"]
                 )
                 df.loc[i, "Kategorie"] = row["Kategorie"]
-                df.loc[i, "Datum"] = pd.to_datetime(row["Datum"])
+                df.loc[i, "Datum"] = pd.to_datetime(row["Datum"]).strftime("%Y-%m-%d")
                 df.loc[i, "Popis závady"] = (
                     str(row["Popis závady"]).strip()
                     if pd.notna(row["Popis závady"])
