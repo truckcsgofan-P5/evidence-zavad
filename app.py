ea4be664bcd8a7d6898db0a1d3fe61fccdbf7e8f
truckcsgofan-P5 <truckcsgofan@gmail.com>
2026-08-23 15:43:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ with tab_prehled:
         height=500,
         num_rows="fixed",
         disabled=["ID"],
-        hide_index=True
+        hide_index=True,
         column_order=["ID", "Lokomotiva", "Kategorie", "Datum", "Popis závady", "Poznámka", "Fotka"],
         column_config={
             "Datum": st.column_config.DateColumn(
