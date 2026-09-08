abdf3f18431a7d8178b3813f689dbe9624ee2d1e
truckcsgofan-P5 <truckcsgofan@gmail.com>
2026-09-08 13:01:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -290,8 +290,7 @@ def prihlaseni_uzivatele():
     with st.form("login_form"):
         uzivatel = st.text_input("Uživatelské jméno:", placeholder="Zadejte uživatelské jméno")
         heslo = st.text_input("Heslo:", type="password", placeholder="Zadejte heslo")
-        zapamatovat = st.checkbox("Zapamatovat si přihlášení (na 30 dní)", , value=True
-        )
+        zapamatovat = st.checkbox("Zapamatovat si přihlášení (na 30 dní)", value=True)
 
         submit_login = st.form_submit_button("Přihlásit se")
 
